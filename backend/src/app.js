65d013de0f4029a60dd1b0b7c646ac75ea471f55
Adebola Desmond <adeboladesmond18@gmail.com>
2026-01-02 15:26:40 +0100

    feat: add CRUD APIs

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,9 +6,11 @@ app.use(express.json());
 
 // routes import
 import userRoutes from "./routes/user_route.js";
+import postRoutes from "./routes/post_route.js";
 
 // routes declaration
 app.use ('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
 
 // example route: http://localhost:4000/api/users/register
 
